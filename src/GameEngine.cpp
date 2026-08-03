8331e1de34fae5b501a5eb2e04b28d413dbdbc7e
LjhUser <51446112@qq.com>
2026-08-03 12:04:15 +0800

    first commit

---
+++ src/GameEngine.cpp
@@ -0,0 +1,50 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main()
+{
+    glfwInit();
+    // 配置OpenGL版本 3.3 Core
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Test", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "窗口创建失败" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // 加载OpenGL函数（必须！）
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "GLAD加载失败" << std::endl;
+        return -1;
+    }
+
+    // 主渲染循环
+    while (!glfwWindowShouldClose(window))
+    {
+        // 清屏颜色
+        glClearColor(1.0f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
